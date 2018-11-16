430748c5f0595d2a9502a042ee3e594e1ce01fb8
longfeigao <jiushi.12345@163.com>
2018-11-16 10:18:35 +0800

    6.0.2 1.delete debug info; 2.optimize upload.bat

--- AP3_Dongle/upload.bat
+++ AP3_Dongle/upload.bat
@@ -1,18 +1,80 @@
-@ECHO OFF
+REM @ECHO OFF
 if "%1"=="" (SET/P tmp=input version:) else (SET tmp=%1)
+if "%2"=="" (
+	SET/P bleflg=°üº¬À¶ÑÀbinÎÄ¼þ£¿y/n:	
+) else (
+	SET bleflg=%2
+)
+
+
+if "%bleflg%" == "y" ( 
+	SET/P BLEBIN=Input bin:
+	SET/P BLEPATH=Input path:
+	SET/P tmp2=input ble version:
+	mv 
+) else (
+	SET BLEBIN= 
+	SET BLEPATH= 
+	SET tmp2=
+)
+
 
 REM echo %tmp%
 
-SET myPatch=%~dp0Debug
+SET myPath=%~dp0Debug
+SET UploadProgramPath=..\UploadProgram
+SET FILENAME=AP3_Dongle.bin
+SET FILENAMEZIP=AP3_Dongle.zip
+
+
 
 REM rename
-mv %myPatch%\AP3_Dongle.bin  %myPatch%\rfd-%tmp%.bin
+if not exist "%UploadProgramPath%\rfd-%tmp%.bin" (
+	mv %myPath%\%FILENAME%  %myPath%\rfd-%tmp%.bin
+) else (
+	cp	%UploadProgramPath%\rfd-%tmp%.bin %myPath%\rfd-%tmp%.bin
+)
+
+if "%bleflg%"=="y" (
+	if not exist "%UploadProgramPath%\ble%tmp2%.bin" (
+		cp %BLEPATH%\%BLEBIN% 		%myPath%\ble%tmp2%.bin
+	) else (
+		cp	%UploadProgramPath%\ble%tmp2%.bin %myPath%\ble%tmp2%.bin
+	)
+) 
 
 REM compress
-"D:\Program Files (x86)\Haozip"\HaoZipC a -tzip %myPatch%\AP3_Dongle.zip %myPatch%\rfd-%tmp%.bin
+
+if "%bleflg%"=="y" (
+"D:\Program Files (x86)\Haozip"\HaoZipC a -tzip %myPath%\%FILENAMEZIP% %myPath%\rfd-%tmp%.bin %myPath%\ble%tmp2%.bin
+) else (
+"D:\Program Files (x86)\Haozip"\HaoZipC a -tzip %myPath%\%FILENAMEZIP% %myPath%\rfd-%tmp%.bin
+)
 
 REM delete *.bin
-del /P %myPatch%\rfd-%tmp%.bin
+REM del /P %myPath%\rfd-%tmp%.bin
+
+REM Create file
+if not exist "%UploadProgramPath%"  md %UploadProgramPath%
+
+REM Copy file
+if not exist "%UploadProgramPath%\rfd-%tmp%.bin"  (
+	mv %myPath%\rfd-%tmp%.bin 	%UploadProgramPath%\rfd-%tmp%.bin
+) else (
+	del /P %myPath%\rfd-%tmp%.bin 
+)
+
+if "%bleflg%"=="y" 	(
+	if "%BLEPATH%\%BLEBIN%"=="%UploadProgramPath%\ble%tmp2%.bin" (
+		del /P %myPath%\ble%tmp2%.bin
+	) else (
+		mv %myPath%\%BLEBIN% 	%UploadProgramPath%\ble%tmp2%.bin	
+	)	
+)
 
 REM open filezilla
-"C:\Program Files (x86)\FileZilla FTP Client"\filezilla ftp://upload:upload@192.168.5.95/rfd_3.0 --local=%myPatch%
+"C:\Program Files (x86)\FileZilla FTP Client"\filezilla ftp://upload:upload@192.168.5.95/rfd_3.0 --local=%myPath%
+
+REM delete the file of xx.zip
+del /P %myPath%\%FILENAMEZIP%
+
